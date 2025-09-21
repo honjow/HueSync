22c7f9f3d2c4dd5f337b63e3aad21627a991c273
Aarron Lee <aarron-lee@users.noreply.github.com>
2025-09-20 21:11:35 -0400

    fix: suspend on steam client via mobx suspend-resume implementation

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,4 @@
-import {
-  definePlugin,
-  PanelSection,
-  staticClasses,
-} from "@decky/ui";
+import { definePlugin, PanelSection, staticClasses } from "@decky/ui";
 import { FC } from "react";
 import { FaLightbulb } from "react-icons/fa";
 
@@ -11,9 +7,12 @@ import { RGBComponent, SuspendModeComponent } from "./components";
 import { Backend } from "./util";
 import { Setting } from "./hooks";
 import { MoreComponent } from "./components/more";
+import {
+  getResumeObservable,
+  getSuspendObservable,
+} from "./sleep/suspendResumeObservables";
 
 const Content: FC = () => {
-
   return (
     <div>
       <PanelSection
@@ -27,31 +26,63 @@ const Content: FC = () => {
   );
 };
 
+const onResume = async () => {
+  setTimeout(() => {
+    Backend.applySettings({ isInit: true });
+    console.log("Resume from suspend");
+  }, 5000);
+};
+
+const onSuspend = async () => {
+  Backend.throwSuspendEvt();
+  console.log("Entering suspend mode");
+};
+
 export default definePlugin(() => {
   const init = async () => {
     await localizationManager.init();
     await Backend.init();
     await Setting.init();
     Backend.applySettings({ isInit: true });
-  }
+  };
 
   init();
 
-  SteamClient.System.RegisterForOnResumeFromSuspend(async () => {
-    setTimeout(() => {
-      Backend.applySettings({ isInit: true });
-      console.log("Resume from suspend");
-    }, 5000);
-  });
+  try {
+    SteamClient.System.RegisterForOnResumeFromSuspend(onResume);
 
-  SteamClient.System.RegisterForOnSuspendRequest(async () => {
-    Backend.throwSuspendEvt();
-    console.log("Entering suspend mode");
-  });
+    SteamClient.System.RegisterForOnSuspendRequest(onSuspend);
+  } catch (e) {
+    console.error(
+      `SteamClient.System suspend/resume hooks no longer available ${e}, fallback to mobx observable`,
+    );
+
+    const suspendObservable = getSuspendObservable();
+    const resumeObservable = getResumeObservable();
+
+    suspendObservable?.observe_((change) => {
+      const { newValue } = change;
+      console.log({ info: `mobX suspend triggered with ${newValue}` });
+      if (!newValue) {
+        return;
+      }
+      onSuspend();
+    });
+
+    resumeObservable?.observe_((change) => {
+      const { newValue } = change;
+      console.log({ info: `mobX resume triggered with ${newValue}` });
+      if (!newValue) {
+        return;
+      }
+      onResume();
+    });
+  }
+
   return {
     title: <div className={staticClasses.Title}>HueSync</div>,
     content: <Content />,
     icon: <FaLightbulb />,
-    onDismount() { },
+    onDismount() {},
   };
 });
